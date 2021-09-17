f9d9a6f3a3d78f1563433128808921f06e1a7f44
sent1nu11 <sent1nu11@gmail.com>
2021-09-16 22:16:05 -0600

    latest

--- main.py
+++ main.py
@@ -1,13 +1,24 @@
 import turtle
+import pandas
 
 screen = turtle.Screen()
 screen.title("U.S. States Game")
 image = "blank_states_img.gif"
 screen.addshape(image)
 turtle.shape(image)
+menu_title = "Guess the State"
+
+correct_answer_count = 0
+
+data_frame = pandas.read_csv("50_states.csv")
+states = data_frame["state"].to_list()
+
+
+answer_state = screen.textinput(title=menu_title, prompt="What's another state's name?")
+if answer_state in states:
+    correct_answer_count = +1
+else:
+    print("incorrect")
 
 
 
-
-answer_state = screen.textinput(title="Guess the State", prompt="What's another state's name?")
-print(answer_state)
