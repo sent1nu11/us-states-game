ff5413da0e5549d00267cbb04bbb9f92f200e867
sent1nu11 <sent1nu11@gmail.com>
2021-09-18 14:46:12 -0600

    finish game

--- main.py
+++ main.py
@@ -11,14 +11,28 @@ menu_title = "Guess the State"
 correct_answer_count = 0
 
 data_frame = pandas.read_csv("50_states.csv")
-states = data_frame["state"].to_list()
-
-
-answer_state = screen.textinput(title=menu_title, prompt="What's another state's name?")
-if answer_state in states:
-    correct_answer_count = +1
-else:
-    print("incorrect")
+all_states = data_frame["state"].to_list()
+guessed_states = []
 
 
 
+while len(guessed_states) < 50:
+    answer_state = screen.textinput(title=f"{len(guessed_states)}/50 States Correct", prompt="What's another state's name?").title()
+
+    if answer_state.capitalize() in all_states:
+        guessed_states.append(answer_state)
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        state_data = data_frame[data_frame.state == answer_state]
+        t.goto(int(state_data["x"]), int(state_data["y"]))
+        t.write(answer_state)
+        correct_answer_count = +1
+
+
+screen.listen()
+
+menu_title = correct_answer_count
+
+
+screen.exitonclick()
