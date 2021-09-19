0690f9ecbefdb088134dc6c4d2fbb2881237995b
sent1nu11 <sent1nu11@gmail.com>
2021-09-19 17:12:23 -0600

    finish

--- main.py
+++ main.py
@@ -19,6 +19,11 @@ guessed_states = []
 while len(guessed_states) < 50:
     answer_state = screen.textinput(title=f"{len(guessed_states)}/50 States Correct", prompt="What's another state's name?").title()
     if answer_state == "Exit":
+        missing_states = set(all_states).difference(guessed_states)
+        new_data = pandas.DataFrame(missing_states)
+        print(missing_states)
+        new_data = pandas.DataFrame(missing_states)
+        new_data.to_csv("states_to_learn.csv")
         break
 
     if answer_state in all_states:
@@ -31,8 +36,7 @@ while len(guessed_states) < 50:
         t.write(answer_state)
 
 # states to learn.csv
-missed_states = set(all_states).difference(guessed_states)
-print(missed_states)
+
 
 
 
